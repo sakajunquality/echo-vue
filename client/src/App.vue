60b90437f450c56654341a7103d6a147e50b6308
Jun SAKATA <jun.bj141400@gmail.com>
2017-03-07 19:14:45 +0900

    viewだけ用意。細かいところは後で調整

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
+
+    <footer class="info">
+      <p>Powered by <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
   </div>
 </template>
 
@@ -10,14 +13,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ client/src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo"
+          autofocus autocomplete="off"
+          placeholder="What needs to be done?">
+      </header>
+      <section class="main">
+        <input class="toggle-all" type="checkbox">
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view"><input type="checkbox" class="toggle"> <label>testtest</label> <button class="destroy"></button></div> <input type="text" class="edit">
+          </li>
+        </ul>
+      </section>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'todo'
+}
+</script>
