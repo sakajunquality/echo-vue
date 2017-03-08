677ea361aaf85e561812dbb7f9efa4ce801ae935
Jun SAKATA <jun.bj141400@gmail.com>
2017-03-08 11:27:15 +0900

    try some routing

---
+++ client/src/components/NotFound.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="NotFound">
+    <h1>{{ msg }}</h1>
+    <h2>Pages</h2>
+    <ul>
+      <li><router-link to="/">Todo</router-link></li>
+      <li><router-link to="/hello">Hello</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Not Found (´・ω・｀)'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Todo from '@/components/Todo'
+import Hello from '@/components/Hello'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Todo',
       component: Todo
+    },
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
