1416b83aa3e553c09b667b5f40b9aaf9e6a475ae
Jun SAKATA <jun.bj141400@gmail.com>
2017-03-09 11:03:25 +0900

    レスポンスをそれっぽくする

--- app.go
+++ app.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 	"strconv"
 
+	"fmt"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/sakajunquality/echo-vue/todo"
 )
 
 var todoList todo.List
+
+type resp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
 
 func main() {
 	todoList = todo.List{}
@@ -37,9 +44,19 @@ func main() {
 func getTask(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Invalid ID specification"})
 	}
-	return c.JSON(http.StatusOK, todoList[id])
+	td, ok := todoList[id]
+	if !ok {
+		return c.JSON(http.StatusNotFound,
+			resp{
+				Code:    http.StatusBadRequest,
+				Message: fmt.Sprintf("ID: %s was not found", strconv.Itoa(id)),
+			})
+	}
+
+	return c.JSON(http.StatusOK, td)
 }
 
 func getList(c echo.Context) error {
@@ -49,40 +66,62 @@ func getList(c echo.Context) error {
 func addTask(c echo.Context) error {
 	title := c.FormValue("title")
 	if title == "" {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Title is not valid"})
 	}
 	todoList.Add(title)
-	return c.JSON(http.StatusOK, "ok") // @todo まともなレスポンス返す
+
+	return c.JSON(http.StatusOK,
+		resp{
+			Code:    http.StatusOK,
+			Message: fmt.Sprintf("New Task %s was added", title),
+		})
 }
 
 func changeTaskStatus(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Invalid ID specification"})
 	}
 	todoList.ChangeStatus(id)
-	return c.JSON(http.StatusOK, "ok") // @todo まともなレスポンス返す
+	return c.JSON(http.StatusOK,
+		resp{
+			Code:    http.StatusOK,
+			Message: fmt.Sprintf("Status of task #%s was changed", strconv.Itoa(id)),
+		})
 }
 
 func editTask(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Invalid ID specification"})
 	}
 	title := c.FormValue("title")
 	if title == "" {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Title is not valid"})
 	}
 
 	todoList.Edit(id, title)
-	return c.JSON(http.StatusOK, "ok") // @todo まともなレスポンス返す
+	return c.JSON(http.StatusOK,
+		resp{
+			Code:    http.StatusOK,
+			Message: fmt.Sprintf("Title of task #%s was changed", strconv.Itoa(id)),
+		})
 }
 
 func deleteTask(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, "invalid request")
+		return c.JSON(http.StatusBadRequest,
+			resp{Code: http.StatusBadRequest, Message: "Invalid ID specification"})
 	}
 	todoList.Remove(id)
-	return c.JSON(http.StatusOK, "ok") // @todo まともなレスポンス返す
+	return c.JSON(http.StatusOK,
+		resp{
+			Code:    http.StatusOK,
+			Message: fmt.Sprintf("Task #%s was deleted", strconv.Itoa(id)),
+		})
 }
